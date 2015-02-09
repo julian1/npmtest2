09ddc7b143f68ca532dd50ce4da19e3eee6506b1
julian1 <julian.atkinson71@gmail.com>
2015-02-09 22:20:44 +1100

    update

---
+++ pg.js
@@ -0,0 +1,20 @@
+
+var pg = require('pg');
+var conString = "postgres://x:x@127.0.0.1/postgres?ssl=true";
+
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+});
+
